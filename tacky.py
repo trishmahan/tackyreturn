086a90355255af9d568511598dc128850d7b0a2c
Trish <trish@maxxpotential.com>
2020-12-16 21:32:12 -0500

    add house landing pages

--- tacky.py
+++ tacky.py
@@ -7,9 +7,18 @@ def tacky_form():
 
 @app.route('/home',methods = ['POST', 'GET'])
 def result():
-   if request.method == 'POST':
-      result = request.form
-      return render_template("home.html",result = result)
+    if request.method == 'POST':
+        result = request.form
+        if result['trait'] == "brave":
+            return render_template("gryf.html",result = result)
+        elif result['trait'] == "loyal":#loyal wise cunning
+            return render_template("huff.html",result = result)
+        elif result['trait'] == "wise":#loyal wise cunning
+            return render_template("rav.html",result = result)
+        elif result['trait'] == "cunning":#loyal wise cunning
+            return render_template("slyth.html",result = result)
+        else:
+            return render_template("home.html",result = result)
 
 # @app.route('/hoggy-hoggy-hogwartz',methods = ['POST', 'GET'])
 # def result():
@@ -18,4 +27,4 @@ def result():
 #       return render_template("home.html",result = result)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
